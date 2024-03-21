516095b649b3da69ae20c8ad8c4b662bc29fbde5
azureqaq <azureqaq@gmail.com>
2024-03-21 15:03:25 +0800

    init

---
+++ src/lib/Version.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { onMount } from "svelte";
+    const endpoint = "/dst-version/version.txt";
+
+    let data = "...";
+
+    onMount(async function () {
+        const response = await fetch(endpoint);
+        const t = await response.text();
+        data = t.trim();
+    });
+</script>
+
+<p>Version: {data}</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Version from "$lib/Version.svelte";
+</script>
+
+<h1>DST Version</h1>
+
+<Version />
